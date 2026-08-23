31929ce945be2a59e104e29bdc0c221c6fb5c4d8
Brian <brianbickersmith@gmail.com>
2026-08-23 18:22:33 -0400

    v1 header fix

--- scripts/footer.js
+++ scripts/footer.js
@@ -2,7 +2,7 @@ export async function loadFooter() {
   const footerContainer = document.getElementById("footer");
   if (!footerContainer) return;
 
-  const res = await fetch("../components/footer.html");
+  const res = await fetch("./components/footer.html");
   const html = await res.text();
   footerContainer.innerHTML = html;
 }
